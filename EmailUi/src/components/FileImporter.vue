cc76e7eb19ba91ba4bd9f3c26ce36c6e6c8bc11f
yerenagmt@gmail.com <Yeren Palacios>
2024-02-07 10:51:39 -0500

    added loading spiner for file importer

--- EmailUi/src/components/FileImporter.vue
+++ EmailUi/src/components/FileImporter.vue
@@ -4,7 +4,7 @@ import { messagesService } from './messagesService';
 
 var importFile = null
 
-const { uploadFile, error } = messagesService()
+const { uploadFile, error, loading } = messagesService()
 const uploaded = ref(false)
 
 const handleUploadFile = (e) => {
@@ -25,6 +25,9 @@ const sendFile = () => {
 </script>
 <template>
     <p v-if="uploaded" class="text-green-300">File uploaded</p>
+    <div v-if="loading" class="">
+        <div class="loader"></div>
+    </div>
     <div v-else class="text-center">
         <input v-on:change="handleUploadFile" class="text-slate-400 m-3 
               file:mr-4 file:py-2 file:px-4 file:cursor-pointer
@@ -34,6 +37,7 @@ const sendFile = () => {
              file:text-blue-700  hover:file:bg-blue-100" type="file">
         <button @click="sendFile" class="bg-blue-100 text-blue-700 hover:bg-blue-200 py-2 px-4 rounded-full">Import
             messages</button>
+
     </div>
     <div v-if="error" class="absolute shadow-md bg-red-500 right-10 bottom-10 px-4 py-2 rounded-sm text-slate-100">Error: {{
         error }}</div>
